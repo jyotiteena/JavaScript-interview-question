b7a9ca9d94c4e17778a3a3f701520f1bd45d2ad0
jyotiteena <jingarjyoti9413@gmail.com>
2025-11-26 14:54:58 +0530

    nested array to single array

---
+++ nested-to-single-array.js
@@ -0,0 +1,35 @@
+let arr = [1, [2, 3], [4, [5, 6]]];
+let result = [];
+
+function flatten(a) {
+    for (let i = 0; i < a.length; i++) {
+        let value = a[i];
+
+        // check if value is array
+        if (Array.isArray(value)) {
+            console.log("if value = ", value);
+            flatten(value); // recursive call
+        } else {
+            console.log("else value = ", value);
+            result[result.length] = value; // manual push
+        }
+    }
+}
+
+flatten(arr);
+
+console.log(result);
+// [1, 2, 3, 4, 5, 6]
+
+
+/////////// loop flow //////////////
+// else value =  1
+// if value =  [ 2, 3 ]
+// else value =  2
+// else value =  3
+// if value =  [ 4, [ 5, 6 ] ]
+// else value =  4
+// if value =  [ 5, 6 ]
+// else value =  5
+// else value =  6
+// [ 1, 2, 3, 4, 5, 6 ]
